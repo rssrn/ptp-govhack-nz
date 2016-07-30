7ef606b2cbe9e53cf0b38fb2fc723e04d8409d2d
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-07-30 22:38:28 +1200

    Tested code

--- chatbot/test.js
+++ chatbot/test.js
@@ -1,15 +1,15 @@
 var watson = require('watson-developer-cloud');
 
 var conversation = watson.conversation({
-  username: '<username>',
-  password: '<password>',
+  username: '',
+  password: '',
   version: 'v1',
-  version_date: '2016-07-01'
+  version_date: '2016-07-11'
 });
 
 conversation.message({
-  input: 'What\'s the weather?',
-  workspace_id: '<workspace id>'
+  input: {text: 'What\'s the weather?'},
+  workspace_id: ''
  }, function(err, response) {
      if (err) {
        console.error(err);
