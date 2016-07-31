a5e5d9542692dc073eb393ec58fc9c12929a4b67
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-07-31 15:26:19 +1200

    Add Watson bot

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var app = express();
+console.log(process.env.IBM_USER);
+// IBM Watson
+var watson = require('watson-developer-cloud');
+var conversation = watson.conversation({
+  username: process.env.IBM_USER,
+  password: process.env.IBM_PASS,
+  version: 'v1',
+  version_date: '2016-07-11'
+});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,6 +24,23 @@ app.get('/', function(request, response) {
 
 app.get('/map', function(request, response) {
   response.render('pages/map');
+});
+
+app.get('/bot', function(request, response) {
+  var query = request.query.q;
+
+  conversation.message({
+      input: {text: query},
+      workspace_id: process.env.WORKSPACE_ID
+    }, function(err, res) {
+      if (err) {
+        return next(err);
+      } else {
+        //console.log(JSON.stringify(response, null, 2));
+        response.json(res);
+      }
+    }
+  );
 });
 
 app.listen(app.get('port'), function() {
--- public/js/ptp.js
+++ public/js/ptp.js
@@ -1,3 +1,80 @@
+function processEvent(e) {
+	var key = (e.keyCode ? e.keyCode : e.which);
+	if (key == 13) {
+		chat();
+	}
+};
+
+function chat() {
+	var $message = $("#message-textarea");
+	var message = $message.val();
+
+	console.log('Asking ' + message);
+
+	var newElement = '<div class="row"><div class="col-lg-12"><div class="media"><a class="pull-left" href="#">' +
+					 '<img class="media-object img-circle" src="http://lorempixel.com/30/30/people/7/" alt="">' +
+                     '</a><div class="media-body"><h4 class="media-heading">Visitor</h4>' +
+					 '<p>' + message + '</p></div>' +
+                     '</div></div></div><hr>';
+
+    var $chatbox = $("#chatbox");
+    $chatbox.append(newElement);
+
+    $message.val('');
+    $chatbox.scrollTop($chatbox.prop('scrollHeight') - $chatbox.height());
+
+	var url = "http://localhost:5000/bot?q=" + message;
+	$.get(url, function(data) {
+		console.log(data);
+    	var intent = data.intents[0].intent;
+		if (intent === 'toilets') {
+			console.log('show toilets');
+			if (!$("#toilets").prop('checked'))
+				$('#toilets').click();
+		} else if (intent == 'walk') {
+			console.log('show walking paths');
+			if (!$("#walkTrack").prop('checked'))
+				$('#walkTrack').click();
+		} else if (intent == 'events') {
+			console.log('show events');
+			if (!$("#event").prop('checked'))
+				$('#event').click();
+		} else if (intent == 'picnic') {
+			console.log('show picnic');
+			if (!$("#picTable").prop('checked'))
+				$('#picTable').click();
+		} else if (intent == 'dog') {
+			console.log('show dog walking');
+			if (!$("#dogEx").prop('checked'))
+				$('#dogEx').click();
+		} else if (intent == 'heritage') {
+			console.log('show heritage');
+			if (!$("#heritage").prop('checked'))
+				$('#heritage').click();
+		} else {
+			console.log('Unknown intent: ' + intent);
+		}
+
+		var botsays = data.output.text[0];
+		console.log(botsays);
+
+		if (typeof botsays === typeof undefined || botsays === '') {
+			botsays = "Sorry, I couldn't understand you. Could you reword it a bit?";
+		}
+
+		var newElement = '<div class="row"><div class="col-lg-12"><div class="media"><a class="pull-left" href="#">' +
+					 '<img class="media-object img-circle" src="http://lorempixel.com/30/30/people/1/" alt="">' +
+                     '</a><div class="media-body"><h4 class="media-heading">Le Bot' + '</h4>' +
+					 '<p>' + botsays + '</p></div>' +
+                     '</div></div></div><hr>';
+
+    	$chatbox.append(newElement);
+    	$chatbox.scrollTop($chatbox.prop('scrollHeight') - $chatbox.height());
+
+    });
+};
+
+
 $(document).ready(function(){
 	$('#main').load('home.html',function(){
 		initMaps();
@@ -6,13 +83,15 @@ $(document).ready(function(){
 		$('.navbar-nav > li ').removeClass('active');
 		$(this).addClass('active')
 		$('#main').load($(this).data('href'));
-	})
+	});
 	$('#main').on('click','.ontheway',function(){
 		loadLayers($(this).val(),$(this).prop('checked'));
-	})
+	});
 	$('#main').on('click','.btn-plan',function(){
 		$('html, body').animate({
             scrollTop:($("#googleMap").offset().top - 50)
         }, 1000);
-	})
+	});
+	
+	$("#main").on('click', '#message-btn', function() { chat(); });
 })
